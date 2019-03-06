a1b31252e12a9903dbb35b64bda0d5d2ce44a0bd
naturulo <ryusuke.sakata.acljr213@gmail.com>
2019-03-06 21:35:31 +0900

    add company-mode setting for elm

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -111,3 +111,19 @@ There are two things you can do about this warning:
 ;;; install elm-mode
 (use-package elm-mode
   :ensure t)
+(add-to-list 'company-backends 'company-elm)
+(add-hook 'elm-mode-hook 'company-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (elm-mode magit alchemist rainbow-delimiters highlight-indent-guides which-key hide-mode-line diminish use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
