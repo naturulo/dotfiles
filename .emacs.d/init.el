3e9e77f40a59608d096ac96aacd078daed5928fd
naturulo <ryusuke.sakata.acljr213@gmail.com>
2019-03-06 11:29:06 +0900

    remove doom and add dracula

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -55,32 +55,8 @@
     (setq after-save-hook
           (cons 'my:delete-file-if-no-contents after-save-hook)))
 
-;;; install doom theme
-(use-package doom-themes
-    :custom
-    (doom-themes-enable-italic t)
-    (doom-themes-enable-bold t)
-    :custom-face
-    (doom-modeline-bar ((t (:background "#6272a4"))))
-    :config
-    (load-theme 'doom-dracula t)
-    (doom-themes-neotree-config)
-    (doom-themes-org-config))
-;;; install doom modeline
-(use-package doom-modeline
-      :custom
-      (doom-modeline-buffer-file-name-style 'truncate-with-project)
-      (doom-modeline-icon t)
-      (doom-modeline-major-mode-icon nil)
-      (doom-modeline-minor-modes nil)
-      :hook
-      (after-init . doom-modeline-mode)
-      :config
-      (line-number-mode 0)
-      (column-number-mode 0)
-      (doom-modeline-def-modeline 'main
-    '(bar workspace-number window-number evil-state god-state ryo-modal xah-fly-keys matches buffer-info remote-host buffer-position parrot selection-info)
-    '(misc-info persp-name lsp github debug minor-modes input-method major-mode process vcs checker)))
+;;; install dravula theme
+(use-package dracula-theme)
 ;;; install hide-mode-line
 (use-package hide-mode-line
     :hook
