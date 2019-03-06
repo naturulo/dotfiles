ff54e76a2e728150361701658861257fb6bcb32c
naturulo <ryusuke.sakata.acljr213@gmail.com>
2019-03-06 13:18:47 +0900

    erase doom and neotree

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,8 +14,6 @@
 (setq linum-format "%5d")
 ;;; don't ring bells
 (setq ring-bell-function 'ignore)
-;;; highlight current row
-(global-hl-line-mode t)
 ;;; highlight parans
 (show-paren-mode t)
 (setq show-paren-style 'mixed)
@@ -76,35 +74,6 @@ There are two things you can do about this warning:
     (setq after-save-hook
           (cons 'my:delete-file-if-no-contents after-save-hook)))
 
-;;;; install doom-theme
-;(use-package doom-themes
-;  :ensure t
-;  :custom
-;  (doom-themes-enable-italic t)
-;  (doom-themes-enable-bold t)
-;  :custom-face
-;  (doom-modeline-bar ((t (:background "#6272a4"))))
-;  :config
-;  (load-theme 'doom-dracula t)
-;  (doom-themes-neotree-config)
-;  (doom-themes-org-config))
-;;;; install doom-modeline
-;(use-package doom-modeline
-;  :ensure t
-;  :custom
-;  (doom-modeline-buffer-file-name-style 'truncate-with-project)
-;  (doom-modeline-icon t)
-;  (doom-modeline-major-mode-icon nil)
-;  (doom-modeline-minor-modes nil)
-;  :hook
-;  (after-init . doom-modeline-mode)
-;  :config
-;  (line-number-mode 0)
-;  (column-number-mode 0)
-;  (doom-modeline-def-modeline 'main
-;  '(bar workspace-number window-number evil-state god-state ryo-modal xah-fly-keys matches buffer-info remote-host buffer-position parrot selection-info)
-;  '(misc-info persp-name lsp github debug minor-modes input-method major-mode process vcs checker)))
-  
 ;;; install hide-mode-line
 (use-package hide-mode-line
   :ensure t
@@ -115,36 +84,6 @@ There are two things you can do about this warning:
   :ensure t
   :diminish which-key-mode
   :hook (after-init . which-key-mode))
-;;; install neotree to view easily directory
-(use-package neotree
-  :ensure t
-  :after
-  projectile
-  :commands
-  (neotree-show neotree-hide neotree-dir neotree-find)
-  :custom
-  (neo-theme 'nerd2)
-  :bind
-  ("<f9>" . neotree-projectile-toggle)
-  :preface
-  (defun neotree-projectile-toggle ()
-    (interactive)
-    (let ((project-dir
-       (ignore-errors
-       ;;; Pick one: projectile or find-file-in-project
-         (projectile-project-root)
-         ))
-      (file-name (buffer-file-name))
-      (neo-smart-open t))
-  (if (and (fboundp 'neo-global--window-exists-p)
-       (neo-global--window-exists-p))
-      (neotree-hide)
-    (progn
-      (neotree-show)
-      (if project-dir
-      (neotree-dir project-dir))
-      (if file-name
-	  (neotree-find file-name)))))))
 ;;; install highlight-indent-guides to view indentation
 (use-package highlight-indent-guides
   :ensure t
@@ -160,24 +99,6 @@ There are two things you can do about this warning:
   :ensure t
   :hook
   (prog-mode . rainbow-delimiters-mode))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(doom-themes-enable-bold t)
- '(doom-themes-enable-italic t)
- '(package-selected-packages
-   (quote
-    (rainbow-delimiters highlight-indent-guides neotree which-key dracula-theme use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(doom-modeline-bar ((t (:background "#6272a4"))))
- '(show-paren-match ((nil (:background "#44475a" :foreground "#f1fa8c")))))
 ;;; install org
 (use-package org
   :ensure t)
