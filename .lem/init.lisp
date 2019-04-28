a8887eb005ed713ec3652e4565cbca85fdb41862
naturulo <ryusuke.sakata.acljr213@gmail.com>
2019-04-28 13:09:14 +0900

    enabled paren-mode and paren-coloring when lisp-mode

--- .lem/init.lisp
+++ .lem/init.lisp
@@ -1,3 +1,14 @@
+(defpackage #:lem-my-init
+  (:use #:cl
+        #:lem))
+(in-package :lem-my-init)
+
 (define-key *global-keymap* "Return" 'lem.language-mode:newline-and-indent)
+
 (setf *scroll-recenter-p* nil)
+(sttf (variable-value 'truncate-lines :global) nil)
+(setf (variable-value 'lem.line-numbers:line-numbers :global) t)
+
+(lem-paredit-mode:paredit-mode)
+(lem-lisp-mode.paren-coloring:toggle-paren-coloring)
 
