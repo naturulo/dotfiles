580ae85f385f30398004e46af9241625626df0b3
naturulo <ryusuke.sakata.acljr213@gmail.com>
2019-04-28 14:22:10 +0900

    Corrected misspell and modified on init.lisp

--- .lem/init.lisp
+++ .lem/init.lisp
@@ -1,7 +1,12 @@
+(defpackage #:lem-my-init
+  (:use #:cl
+        #:lem))
+(in-package :lem-my-init)
+
 (define-key *global-keymap* "Return" 'lem.language-mode:newline-and-indent)
 
 (setf *scroll-recenter-p* nil)
-(sttf (variable-value 'truncate-lines :global) nil)
+(setf (variable-value 'truncate-lines :global) nil)
 (setf (variable-value 'lem.line-numbers:line-numbers :global) t)
 
 (lem-paredit-mode:paredit-mode)
